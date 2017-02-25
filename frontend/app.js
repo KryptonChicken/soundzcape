db617017397c480ff4f93fe5582bc425dd1dbdb1
Tommy Ip <hkmp7tommy@gmail.com>
2017-02-25 18:09:59 +0000

    webpack: Add babel (ES6/ES2015) loader.
    This allows our frontend code to be written in ES2015 and get
    transpiled into ES5 by Webpack.

--- frontend/app.js
+++ frontend/app.js
@@ -1,2 +1,20 @@
-var elem = document.getElementById("js-status");
-elem.innerHTML = "Javascript working";
+// ES6 module import/export check
+import utils from "./utils";
+
+const js_status = document.getElementById("js-status");
+const js_es6_check = document.getElementById("js-es6-check");
+const js_module_check = document.getElementById("js-module-check");
+
+const es6 = "ES2015/ES6";
+const x = 5;
+const y = 10;
+
+js_status.innerHTML = "Javascript working!";
+
+// Check if es6 template literal works.
+js_es6_check.innerHTML = `JS version: ${es6}`;
+
+let add_result = utils.add(x, y);
+let arrow_add_result = utils.arrow_add(x, y);
+
+js_module_check.innerHTML = `ES6 modules working: ${add_result == arrow_add_result}`;
---
+++ frontend/utils.js
@@ -0,0 +1,12 @@
+function add(a, b) {
+    return a + b;
+}
+
+let arrow_add = (a, b) => a + b;
+
+let utils = {
+    add,
+    arrow_add
+};
+
+export default utils;
