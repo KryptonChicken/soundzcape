2002389f24772b6552a00c6a2c33e11afdc028c7
Tommy Ip <hkmp7tommy@gmail.com>
2017-03-07 18:17:46 +0000

    backend: Add JSON response abstraction.

---
+++ backend/rest_response.py
@@ -0,0 +1,16 @@
+from sanic.response import json
+
+
+def ok(**key_values):
+    """ Construct a JSON response with keyword arguments. """
+
+    return json({"status": "ok", **key_values})
+
+
+def error(reason="An error occured, please try again later."):
+    """ Construct a JSON error response with reason. """
+
+    return json(dict(
+        status="error",
+        reason=reason
+    ))
--- backend/tests/test_endpoints.py
+++ backend/tests/test_endpoints.py
@@ -1,7 +1,13 @@
-from backend.server import app
+from server import app
 
 
-def test_index():
+def test_api():
     request, response = app.test_client.get("/api")
     assert response.status == 200
-    assert response.body == b'{"version":"0.1.0","status":"ok"}'
+    assert response.body == b'{"status":"ok","version":"0.1.0"}'
+
+
+def test_api_music():
+    request, response = app.test_client.get("/api/music")
+    assert response.status == 200
+    assert b"ok" in response.body
