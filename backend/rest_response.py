1e5cd06b6f134fef5959776afe147b4a12df8961
Tommy Ip <hkmp7tommy@gmail.com>
2017-03-08 22:35:35 +0000

    linting: Switch from flake8 to pylint & fix errors.
    Use pylint as our linter instead of flake8 and fix all lints.
    Also change double quotes to single quotes.
    
    Fixes #43.

--- backend/rest_response.py
+++ backend/rest_response.py
@@ -1,16 +1,23 @@
+"""
+backend/rest_response.py
+~~~~~~~~~~~~~~~~~~~~~~~~
+
+Helper functions for REST endpoints.
+"""
+
 from sanic.response import json
 
 
 def ok(**key_values):
     """ Construct a JSON response with keyword arguments. """
 
-    return json({"status": "ok", **key_values})
+    return json({'status': 'ok', **key_values})
 
 
-def error(reason="An error occured, please try again later."):
+def error(reason='An error occured, please try again later.'):
     """ Construct a JSON error response with reason. """
 
     return json(dict(
-        status="error",
+        status='error',
         reason=reason
     ))
--- backend/server.py
+++ backend/server.py
@@ -13,21 +13,21 @@ import os
 from sanic import Sanic
 from rest_response import ok, error
 
-
-app = Sanic(__name__)
-app.config.LOGO = None
-
 CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
 MUSIC_PATH = os.path.join(CURRENT_PATH, 'music')
 
+app = Sanic(__name__)  # pylint: disable=invalid-name
+app.config.LOGO = None
 
-@app.route("/api")
-async def index(requests):
-    return ok(version="0.1.0")
+@app.route('/api')
+async def index(_requests):
+    """ Index endpoint. """
+    return ok(version='0.1.0')
 
 
-@app.route("/api/music")
-async def music(requests):
+@app.route('/api/music')
+async def music(_requests):
+    """ Return an JSON array of music. """
     try:
         # Use a in-memory DB to store this data when available.
         return ok(music_list=os.listdir(MUSIC_PATH))
@@ -37,7 +37,7 @@ async def music(requests):
         return error()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     # Sanic is not exposed to the internet directly, all traffic to /api
     # should be routed here by NGINX.
-    app.run(host="0.0.0.0", port=8001, debug=True)
+    app.run(host='0.0.0.0', port=8001, debug=True)
