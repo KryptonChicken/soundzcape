dce9d3fc3d6fd4d621d7a1b031c6cfb3b106bba3
Tommy Ip <hkmp7tommy@gmail.com>
2017-03-08 22:35:35 +0000

    linting: Switch from flake8 to pylint & fix errors.
    Use pylint as our linter instead of flake8 and fix all lints.
    Also change double quotes to single quotes.
    
    Fixes #43.

--- backend/tests/test_endpoints.py
+++ backend/tests/test_endpoints.py
@@ -1,13 +1,18 @@
+"""
+tests/test_endpoints.py
+~~~~~~~~~~~~~~~~~~~~~~~
+"""
+
 from server import app
 
 
 def test_api():
-    request, response = app.test_client.get("/api")
+    _request, response = app.test_client.get('/api')
     assert response.status == 200
     assert response.body == b'{"status":"ok","version":"0.1.0"}'
 
 
 def test_api_music():
-    request, response = app.test_client.get("/api/music")
+    _request, response = app.test_client.get('/api/music')
     assert response.status == 200
-    assert b"ok" in response.body
+    assert b'ok' in response.body
